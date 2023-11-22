0557545a925ba7b65a313f7e549ac0b57b9e67db
jimmybombimmy <jamespeters92@gmail.com>
2023-11-22 13:47:41 +0000

    tidy seed file

--- src/config/seed.ts
+++ src/config/seed.ts
@@ -1,16 +1,10 @@
-import { app } from '../app.js';
+
 import { connection } from '../config/database.js'
 import { users } from '../data/test-data/users.js'
 import { transactions } from '../data/test-data/transactions.js';
-import request from "supertest";
 
 const User = connection.models.User
 const Transaction = connection.models.Transaction
-
-interface databaseTables {
-  usersData: object[];
-  transactionsData: object[];
-}
 
 export default async () => {
   await User.deleteMany()
